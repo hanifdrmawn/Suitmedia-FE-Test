f0e9cddc7a4e9b212f4f23997ce043bc334e8599
Hanif Darmawan <darmawanhanif.hd@gmail.com>
2023-12-23 15:40:55 +0700

    commit

--- src/component/content.js
+++ src/component/content.js
@@ -53,54 +53,12 @@ function CoffeeData() {
     return (
         
         <div className="content m-16">
-            <div className="filter flex justify-between">
-                <div className="showing">
-                    <p className='m-auto'>Showing 1-<span id="show-end">10</span> of 20</p>
-                </div>
-                <div className="input flex gap-6">
-                    <div className="show flex gap-1">
-                        <p className="m-auto">Show per page:</p>
-                        <div className="dropdown border rounded-full" id="dropdown">
-                            <button id="dropbtn" className="dropbtn cursor-pointer text-base py-1 px-3">10</button>
-                            <div className="dropdown-content cursor-pointer" id="dropdown-content">
-                                <a onClick={() => changeTextShow(10)}>10</a>
-                                <a onClick={() => changeTextShow(20)}>20</a>
-                                <a onClick={() => changeTextShow(50)}>50</a>
-                            </div>
-                        </div>
-                    </div>
-                    {/* <div className="sort flex gap-1">
-                        <p className="m-auto">Sort by:</p>
-                        <div className="dropdown border rounded-full" id="dropdown">
-                            <button id="dropbuttondrop" className="dropbtn cursor-pointer text-base py-1 px-3 w-24">Newest</button>
-                            <div className="dropdown-content cursor-pointer" id="dropdown-content">
-                                <a onclick="changeTextSort('Newest')">Newest</a>
-                                <a onclick="changeTextSort('Oldest')">Oldest</a>
-                            </div>
-                        </div>
-                    </div> */}
-                </div>
-            </div>
             <div id="content" class="content-main">
                 <CoffeeList />
             </div>
             <div class="font-center m-auto text-center">
-                <div class="pagination text-center">
-                    <a href="#">&laquo;</a>
-                    <a class="active" id="pag1" href="#">1</a>
-                    <a id="pag2" className='cursor-pointer'>2</a>
-                    <a id="pag3" className='cursor-pointer'>3</a>
-                    <a href="#">&raquo;</a>
-                </div>
             </div>
         </div>
     );
     
-}
-
-const changeTextShow = (a) => {
-    var dropbtn = document.getElementById('dropbtn');
-    var showend = document.getElementById('show-end');
-    dropbtn.innerText = a;
-    showend.innerText = a;
 }
--- src/data/coffeeData.js
+++ src/data/coffeeData.js
@@ -5,21 +5,21 @@ import '../App.css';
 import '../script.js';import { getAllDataCoffee } from '../data/Coffee';
 import '../component/content.js';
 
-
-
 function CoffeeList() {
+    const [itemsPerPage, setItemsPerPage] = useState(10);
     const [coffeeData, setCoffeeData] = useState([]);
-    const [search, setSearch] = useState('');
     const [count, setCount] = useState(0);
     const [currentPage, setCurrentPage] = useState(1);
     const [postsPerPage] = useState(10);
-
+    const indexOfLastItem = currentPage * itemsPerPage;
+    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+    const currentCoffeeData = coffeeData.slice(indexOfFirstItem, indexOfLastItem);
     const indexOfLastPost = currentPage * postsPerPage;
     const indexOfFirstPost = indexOfLastPost - postsPerPage;
     const currentPosts = coffeeData.slice(indexOfFirstPost, indexOfLastPost);
     const paginate = (pageNumber) => setCurrentPage(pageNumber);
+    const totalPages = Math.ceil(coffeeData.length / itemsPerPage);
     
-
     useEffect(() => {
         async function fetchData() {
             try {
@@ -33,42 +33,82 @@ function CoffeeList() {
 
         fetchData();
     }, []);
-    
+
+    const changeTextShow = (a) => {
+        var dropbtn = document.getElementById('dropbtn');
+        var showend = document.getElementById('show-end');
+        dropbtn.innerText = a;
+        showend.innerText = a;
+        setItemsPerPage(a);
+        setCurrentPage(1);
+    }
+
+    const changeTextSort = (order) => {
+        var dropbuttondrop = document.getElementById('dropbuttondrop');
+        if(order=='asc') {
+            dropbuttondrop.innerText = 'A - Z';
+        }
+        else if(order=='desc') {
+            dropbuttondrop.innerText = 'Z - A';
+        }
+    }
+
     return (
         <div className='content-main'>
-            <div id="content" className="columns-4 my-10 max-md:columns-2 max-sm:columns-1">
-                {coffeeData.map((coffee) => (
-                    <div key={coffee.id} className="content-box break-inside-avoid rounded border h-[350px] shadow-lg mb-6">
-                        <img className="rounded-t w-full max-h-40 object-cover" src={coffee.image} alt={coffee.title} />
-                        <div className="content-text m-4">
-                            <h2 className="font-medium text-xl">{coffee.title}</h2>
-                            <p className="text-gray-400 line-clamp-3">{coffee.description}</p>
+            <div className='filter flex justify-between'>
+            <div className="showing">
+                    <p className='m-auto'>Showing 1-<span id="show-end">10</span> of {count}</p>
+                </div>
+                <div className="input flex gap-6">
+                    <div className="show flex gap-1">
+                        <p className="m-auto">Show per page:</p>
+                        <div className="dropdown border rounded-full" id="dropdown">
+                            <button id="dropbtn" className="dropbtn cursor-pointer text-base py-1 px-3">10</button>
+                            <div className="dropdown-content cursor-pointer" id="dropdown-content">
+                                <a onClick={() => changeTextShow(10)}>10</a>
+                                <a onClick={() => changeTextShow(20)}>20</a>
+                                <a onClick={() => changeTextShow(50)}>50</a>
+                            </div>
                         </div>
+                    </div>
+                    <div className="sort flex gap-1">
+                        <p className="m-auto">Sort by:</p>
+                        <div className="dropdown border rounded-full" id="dropdown">
+                            <button id="dropbuttondrop" className="dropbtn cursor-pointer text-base py-1 px-3 w-24">A - Z</button>
+                            <div className="dropdown-content cursor-pointer" id="dropdown-content">
+                                <a onClick={() => changeTextSort('asc')}>A - Z</a>
+                                <a onClick={() => changeTextSort('desc')}>Z - A</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div id="content" className="columns-4 my-10 max-md:columns-2 max-sm:columns-1">
+                {currentCoffeeData.map((coffee) => (
+                    <div key={coffee.id} className="content-box break-inside-avoid rounded border h-[350px] shadow-lg mb-6">
+                    <img className="rounded-t w-full max-h-40 object-cover" src={coffee.image} alt={coffee.title} />
+                    <div className="content-text m-4">
+                        <h2 className="font-medium text-xl">{coffee.title}</h2>
+                        <p className="text-gray-400 line-clamp-3">{coffee.description}</p>
+                    </div>
                     </div>
                 ))}
             </div>
-            {/* <div className='pagination w-full'>
-                {search === "" ? 
-                <Pagination class="m-auto justify-center" style={{display:'flex'}}>
-                    {
-                        currentPage === 1 ? null : <Pagination.First onClick={() => paginate(1)} />
-                    }
-                    {
-                        currentPage === 1 ? null : <Pagination.Prev onClick={() => paginate(currentPage - 1)} />
-                    }
-                    {Array.from({ length: Math.ceil(count / postsPerPage) }, (_, i) => (
-                        <Pagination.Item key={i + 1} active={i + 1 === currentPage} onClick={() => paginate(i + 1)}>
+            <div className='font-center m-auto text-center'>
+                <div className="pagination text-center">
+                    <button onClick={() => paginate(currentPage - 1)} disabled={currentPage === 1}>
+                        &laquo;
+                    </button>
+                    {Array.from({ length: totalPages }, (_, i) => (
+                        <button key={i + 1} onClick={() => paginate(i + 1)} className={currentPage === i + 1 ? 'active' : ''}>
                             {i + 1}
-                        </Pagination.Item>
+                        </button>
                     ))}
-                    {
-                        currentPage === Math.ceil(count / postsPerPage) ? null : <Pagination.Next onClick={() => paginate(currentPage + 1)} />
-                    }
-                    {
-                        currentPage === Math.ceil(count / postsPerPage) ? null : <Pagination.Last onClick={() => paginate(Math.ceil(count / postsPerPage))} />
-                    }
-                </Pagination> : null}
-            </div> */}
+                    <button onClick={() => paginate(currentPage + 1)} disabled={currentPage === totalPages}>
+                        &raquo;
+                    </button>
+                </ div>
+            </div>
         </div>
     );
 }
